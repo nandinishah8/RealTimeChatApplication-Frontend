3fec2e1b0336a2531e0b1f60d620b4baf20bea38
nandinishah8 <nandinishah8501@gmail.com>
2023-08-09 17:08:22 +0530

    created service for social login

--- ChatApp/src/app/services/social-login.service.ts
+++ ChatApp/src/app/services/social-login.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { UserService } from './user.service';
+import { SocialUser } from '@abacritt/angularx-social-login';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SocialLoginService {
+  constructor(private httpClient: HttpClient) {}
+  url = 'http://localhost:5243/api/SocialLogin';
 
-  constructor() { }
+  sendSocialToken(token: string): Observable<any> {
+    const body = {
+      TokenId: token,
+    };
+    // Send the token to the backend
+    return this.httpClient.post(this.url, body);
+  }
 }

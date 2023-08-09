a993319fde660387b730eea7a53daeed91204be2
nandinishah8 <nandinishah8501@gmail.com>
2023-08-09 12:18:55 +0530

    Google signin  added

--- ChatApp/src/app/pages/login/login.component.ts
+++ ChatApp/src/app/pages/login/login.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { UserService } from 'src/app/services/user.service';
+import { SocialAuthService } from '@abacritt/angularx-social-login';
+import { GoogleLoginProvider } from '@abacritt/angularx-social-login';
+import { SocialUser } from '@abacritt/angularx-social-login';
 import {
   FormGroup,
   FormBuilder,
@@ -14,17 +17,28 @@ import {
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   loginForm!: FormGroup;
+  private accessToken = '';
+  socialuser!: SocialUser;
+  loggedIn!: boolean;
 
   constructor(
     private user: UserService,
     private router: Router,
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private authService: SocialAuthService,
+    private httpClient: HttpClient
   ) {
     this.loginForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required],
+    });
+  }
+
+  ngOnInit() {
+    this.authService.authState.subscribe((user) => {
+      console.log(user.idToken);
     });
   }
 
@@ -49,5 +63,32 @@ export class LoginComponent {
   }
   getControl(name: any): AbstractControl | null {
     return this.loginForm.get(name);
+  }
+
+  refreshToken(): void {
+    this.authService.refreshAuthToken(GoogleLoginProvider.PROVIDER_ID);
+  }
+
+  getAccessToken(): void {
+    this.authService
+      .getAccessToken(GoogleLoginProvider.PROVIDER_ID)
+      .then((accessToken) => (this.accessToken = accessToken));
+  }
+
+  getGoogleCalendarData(): void {
+    if (!this.accessToken) return;
+
+    this.httpClient
+      .get('https://www.googleapis.com/calendar/v3/calendars/primary/events', {
+        headers: { Authorization: `Bearer ${this.accessToken}` },
+      })
+      .subscribe((events) => {
+        alert('Look at your console');
+        console.log('events', events);
+      });
+  }
+
+  refreshAccessToken(): void {
+    this.authService.refreshAccessToken(GoogleLoginProvider.PROVIDER_ID);
   }
 }
